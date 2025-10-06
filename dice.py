7f280e63c28a96b3f31434dd18f6275ef509fa0e
Omar Fayyad <Ofayyad1@outlook.com>
2025-10-06 13:39:47 +0200

    dice

---
+++ dice.py
@@ -0,0 +1,13 @@
+import random
+
+
+
+#User prompt to roll the dice
+
+input("Press Enter to roll the dice...")
+
+# Roll the dice
+result = random.randint(1, 20)
+
+# Show the result
+print("You rolled a", result, "for a 20 sided dice instead of 6.")
