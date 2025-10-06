5c105b6ea5927f75fbf64af1cd55b0196f4c1015
Omar Fayyad <Ofayyad1@outlook.com>
2025-10-06 13:37:56 +0200

    buuged-multiplayer

---
+++ bugged_mp_BoD.py
@@ -0,0 +1,10 @@
+## Figure 7 Code snippet
+
+for i in range(number_of_players):
+   player = copy.deepcopy(player_info)
+player ["name"] = input(f"Enter name for player {i+1}: ")
+player ["email"] = input(f"Enter email for player {i+1}: ")
+player ["country"] = input(f"Enter country for player {i+1}: ")
+
+players.append(player)
+## End of Figure 7 Code snippet
